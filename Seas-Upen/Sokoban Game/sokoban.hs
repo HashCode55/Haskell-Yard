208e02181f41784e94ebbeeebdc9db0e8e029b67
Mehul Ahuja <blackdragon0909@gmail.com>
2017-01-09 22:14:17 +0530

    event handling

--- Seas-Upen/Sokoban Game/sokoban.hs
+++ Seas-Upen/Sokoban Game/sokoban.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import CodeWorld
 
 main :: IO()
@@ -8,41 +10,101 @@ main = sokoban
 -- ## THE BUILDING BLOCKS ## -- 
 wall, ground, storage, box :: Picture 
 
-wall = colored blue (solidRectangle 1 1) -- 0
-ground = colored green (solidRectangle 1 1) -- 1
-storage = colored black (solidCircle 0.5) -- 2
-box = colored brown (solidRectangle 1 1) -- 3
-
+wall = colored (grey 0.4) (solidRectangle 1 1) 
+ground = colored yellow (solidRectangle 1 1) 
+storage = colored black (solidCircle 0.3) & ground 
+box = colored brown (solidRectangle 1 1) 
+player = colored green (solidCircle 0.5) & ground -- the main player
 
 -- ## Drawing the tiles ## --
-drawTile :: Integer -> Picture 
-drawTile x 
-  | x == 1 = wall 
-  | x == 2 = ground
-  | x == 3 = storage
-  | x == 4 = box 
-  | x == 0 = blank 
+data Tile = Wall  -- This is an enum!
+          | Ground 
+          | Storage 
+          | Box 
+          | Blank  
+          | Player 
+            deriving(Eq, Ord, Show)
 
--- ## Maze functio ## --
-maze :: Double -> Double -> Integer
+drawTile :: Tile -> Picture 
+drawTile x 
+  | x == Wall       = wall 
+  | x == Ground     = ground
+  | x == Storage    = storage
+  | x == Box        = box 
+  | x == Player     = player
+  | otherwise       = blank 
+
+-- ## Maze function ## --
+maze :: Integer -> Integer -> Tile
 maze x y
-  | abs x > 4  || abs y > 4  = 0
-  | abs x == 4 || abs y == 4 = 1
-  | x ==  2 && y <= 0        = 1
-  | x ==  3 && y <= 0        = 3
-  | x >= -2 && y == 0        = 4
-  | otherwise                = 2
+  | abs x == 0 && y == 1     = Player 
+  | abs x > 4  || abs y > 4  = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x ==  2 && y <= 0        = Wall
+  | x ==  3 && y <= 0        = Storage 
+  | x >= -2 && y == 0        = Box
+  | otherwise                = Ground
   
   
 -- ## Draw the maze ## --
+
+wrapper :: (Integer -> Picture) -> Picture 
+wrapper func = go (-10) -- func is a captured variable, no need to write it again 
+  where 
+    go 11 = blank 
+    go x = func x & go (x + 1)    
+    
+-- for drawing the tile 
+drawTileAt :: Integer -> Integer -> Picture
+drawTileAt x y = translated (fromIntegral x) (fromIntegral y) (drawTile (maze x y))
+
 pictureOfMaze :: Picture 
-pictureOfMaze = let drawCell x 10 = translated x 10 (drawTile (maze x 10))
-                    drawCell x y = (translated x y (drawTile (maze x y))) & drawCell x (y + 1)
-                    
-                    drawRow 10 y = drawCell 10 y
-                    drawRow x y = (drawCell x y) & drawRow (x + 1) y
-                in  drawRow (-10) (-10)
+pictureOfMaze = wrapper drawRow 
+-- more compact using lambda functions 
+-- pictureOfMaze = wrapper (\r -> wrapper (drawTileAt r))
+drawRow x = wrapper (drawTileAt x) -- Currying
+
+-- ## Event Handling ## --
+
+data Direction = R -- Right
+               | L -- Left
+               | U -- Up
+               | D -- Down 
+               deriving(Eq, Ord, Show)
+               
+data Coord = Coord Integer Integer -- Coordinates. This is a product type datatype
+
+tileCoord :: Coord -> Picture -> Picture 
+tileCoord (Coord x y) pic = translated (fromIntegral x) (fromIntegral y) pic -- Integer makes the code bug free 
+
+-- Lets move the player 
+movePlayer :: Coord -> Direction -> Coord
+movePlayer (Coord x y) dir 
+  | dir == R = Coord (x + 1) y
+  | dir == L = Coord (x - 1) y
+  | dir == U = Coord x (y + 1)
+  | dir == D = Coord x (y - 1)
   
-  
+player_wor = 
+
+handleTime :: Double -> Coord -> Coord -- Coord is the 'world' here
+handleTime _ c = c -- we don't want to use this function, it modifies the state after certain amount of time is passed 
+
+handleEvent :: Event -> Coord -> Coord 
+handleEvent (KeyPress key) c -- handles the input event 
+  | key == "Right" = movePlayer c R 
+  | key == "Left" = movePlayer c L
+  | key == "Up" = movePlayer c U 
+  | key == "Down" = movePlayer c D 
+  | otherwise = c 
+   
+drawState :: Coord -> Picture 
+drawState c = tileCoord c pictureOfMaze -- redraws the picture 
+
 sokoban ::IO()
-sokoban = drawingOf pictureOfMaze
+sokoban = interactionOf initialCoord handleTime handleEvent drawState 
+
+-- experi = translated 0 0 (solidCircle 1) & path [(0, 0), (0, (-3))]
+
+-- experiment_shapes :: IO()
+-- experiment_shapes = drawingOf experi
